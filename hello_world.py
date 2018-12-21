a92d44a1379271ce0e4c00e9b1097e0fec034bbf
kmrsimkhada <kmrsimkhada@gmail.com>
2018-12-21 11:49:49 +0200

    First python code, Hello World

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+'''
+Traditionally the first program in any programming course is the one where the system is made to print the words "Hello World!", in one variation or another. 
+So unsurprisingly the first programming exercise in this course does exactly that. 
+In the last pages of the chapter there were detailed instructions on how to test the interpreter installation and make the first program, which printed some text. 
+In this exercise, the aim is to replicate that example.
+
+So in short, by following the instructions from the chapter, make a program which prints the following text:
+
+Hello World!
+This here is a Python program.
+'''
+
+print("Hello World!")
+print("This here is a Python program.")
