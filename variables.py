5998cdf1d6c1f234ac0905031fde16c3e93ff17d
kmrsimkhada <kmrsimkhada@gmail.com>
2018-12-21 12:15:39 +0200

    Variables in python

---
+++ variables.py
@@ -0,0 +1,15 @@
+#Defining and counting variables
+
+'''
+n this exercise, define two variables and assign them values of 1000 and 24. 
+After this, make the interpreter do the calculation number1+number2+15 and then take the 2nd exponent (**2) of the result. 
+Save this final result to a new variable, and make the program print the result in the following way:
+
+
+The final results of the calculation was: 1079521
+'''
+
+number1 = 1000
+number2 = 24
+result = (number1+number2+15)**2
+print("The final results of the calculation was: "+str(result))
